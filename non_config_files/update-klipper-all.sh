586b2cc0c0b687e71d05e1b44655eb47da46da49
Jadecky <kyle@jadecky.com>
2023-11-12 16:03:56 +0000

    Autocommit from 2023-11-12 16:03:56
    Klipper on commit: 01ed809
    
    Moonraker on commit: b63ca44
    
    Mainsail version: v2.8.0

---
+++ non_config_files/update-klipper-all.sh
@@ -0,0 +1,79 @@
+cd ~/klipper
+
+echo -e -n "\e[0;31mStopping Klipper Service. "
+echo -e -n '\e[0;0m'
+
+sudo systemctl stop klipper
+sudo systemctl status klipper
+
+################################################################################
+
+git pull
+
+################################################################################
+
+# Flash main MCU - BTT Otopus v1.1
+make clean KCONFIG_CONFIG=klipper-bigtreetech-octopus.config
+make menuconfig KCONFIG_CONFIG=klipper-bigtreetech-octopus.config
+make -j4 KCONFIG_CONFIG=klipper-bigtreetech-octopus.config
+
+echo -e -n "\e[0;33mBTT Octopus MCU firmware built, please check above for any errors. "
+echo -e -n "\e[0;33mPress [Enter] to continue flashing, or [Ctrl+C] to abort"
+echo -e -n '\e[0;0m'
+read
+
+make flash FLASH_DEVICE=/dev/serial/by-id/usb-Klipper_stm32f446xx_25004D000F50535556323420-if00 KCONFIG_CONFIG=klipper-bigtreetech-octopus.config
+make flash FLASH_DEVICE=/dev/serial/by-id/usb-CanBoot_stm32f446xx_25004D000F50535556323420-if00 KCONFIG_CONFIG=klipper-bigtreetech-octopus.config
+
+echo -e -n "\e[0;33mBTT Octopus MCU firmware flashed, please check above for any errors. "
+echo -e -n "\e[0;33mPress [Enter] to continue flashing, or [Ctrl+C] to abort"
+echo -e -n '\e[0;0m'
+read
+
+################################################################################
+
+# Flash secondary MCU - BTT EBB36 v1.2
+make clean KCONFIG_CONFIG=klipper-btt-ebb36-v1.2.config
+make menuconfig KCONFIG_CONFIG=klipper-btt-ebb36-v1.2.config
+make -j 4 KCONFIG_CONFIG=klipper-btt-ebb36-v1.2.config
+
+echo -e -n "\e[0;33mBTT EBB36 v1.2 MCU firmware built, please check above for any errors. "
+echo -e -n "\e[0;33mPress [Enter] to continue flashing, or [Ctrl+C] to abort"
+echo -e -n '\e[0;0m'
+read
+
+make flash FLASH_DEVICE=/dev/serial/by-id/usb-Klipper_stm32g0b1xx_40004B001750425539393020-if00 KCONFIG_CONFIG=klipper-btt-ebb36-v1.2.config
+make flash FLASH_DEVICE=/dev/serial/by-id/usb-CanBoot_stm32g0b1xx_40004B001750425539393020-if00 KCONFIG_CONFIG=klipper-btt-ebb36-v1.2.config
+
+echo -e -n "\e[0;33mBTT EBB36 v1.2 MCU firmware flashed, please check above for any errors. "
+echo -e -n "\e[0;33mPress [Enter] to continue flashing, or [Ctrl+C] to abort"
+echo -e -n '\e[0;0m'
+read
+
+################################################################################
+
+# Flash Host MCU - Raspberry Pi
+make clean KCONFIG_CONFIG=klipper-raspberry-pi.config
+make menuconfig KCONFIG_CONFIG=klipper-raspberry-pi.config
+make -j 4 KCONFIG_CONFIG=klipper-raspberry-pi.config
+
+echo -e -n "\e[0;33mRaspberry Pi MCU firmware flashed, please check above for any errors. "
+echo -e -n "\e[0;33mPress [Enter] to continue flashing, or [Ctrl+C] to abort"
+echo -e -n '\e[0;0m'
+read
+
+make flash KCONFIG_CONFIG=klipper-raspberry-pi.config
+
+echo -e -n "\e[0;33mRaspberry Pi MCU firmware flashed, please check above for any errors. "
+echo -e -n "\e[0;33mPress [Enter] to continue flashing, or [Ctrl+C] to abort"
+echo -e -n '\e[0;0m'
+read
+
+################################################################################
+
+echo -e -n "\e[0;32mStarting Klipper Service."
+echo -e -n '\e[0;0m'
+sudo systemctl start klipper
+sudo systemctl status klipper
+
+exit 0
