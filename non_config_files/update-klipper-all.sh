5314eb7ed853ca07eff5fbe0b9baa56ff97ecf1b
Jadecky <kyle@jadecky.com>
2024-02-25 00:10:08 +0000

    Scripted auto-commit on change (2024-02-25 00:10:08) by gitwatch.sh

--- non_config_files/update-klipper-all.sh
+++ non_config_files/update-klipper-all.sh
@@ -23,7 +23,7 @@ echo -e -n '\e[0;0m'
 read
 
 make flash FLASH_DEVICE=/dev/serial/by-id/usb-Klipper_stm32f446xx_25004D000F50535556323420-if00 KCONFIG_CONFIG=klipper-bigtreetech-octopus.config
-make flash FLASH_DEVICE=/dev/serial/by-id/usb-CanBoot_stm32f446xx_25004D000F50535556323420-if00 KCONFIG_CONFIG=klipper-bigtreetech-octopus.config
+make flash FLASH_DEVICE=/dev/serial/by-id/usb-katapult_stm32f446xx_25004D000F50535556323420-if00 KCONFIG_CONFIG=klipper-bigtreetech-octopus.config
 
 echo -e -n "\e[0;33mBTT Octopus MCU firmware flashed, please check above for any errors. "
 echo -e -n "\e[0;33mPress [Enter] to continue flashing, or [Ctrl+C] to abort"
@@ -43,7 +43,7 @@ echo -e -n '\e[0;0m'
 read
 
 make flash FLASH_DEVICE=/dev/serial/by-id/usb-Klipper_stm32g0b1xx_40004B001750425539393020-if00 KCONFIG_CONFIG=klipper-btt-ebb36-v1.2.config
-make flash FLASH_DEVICE=/dev/serial/by-id/usb-CanBoot_stm32g0b1xx_40004B001750425539393020-if00 KCONFIG_CONFIG=klipper-btt-ebb36-v1.2.config
+make flash FLASH_DEVICE=/dev/serial/by-id/usb-katapult_stm32g0b1xx_40004B001750425539393020-if00 KCONFIG_CONFIG=klipper-btt-ebb36-v1.2.config
 
 echo -e -n "\e[0;33mBTT EBB36 v1.2 MCU firmware flashed, please check above for any errors. "
 echo -e -n "\e[0;33mPress [Enter] to continue flashing, or [Ctrl+C] to abort"
